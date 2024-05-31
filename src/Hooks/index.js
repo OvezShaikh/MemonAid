09c0b88a3f9f569ec337c262a5113c33d6812cda
Ovez Shaikh <ovezshaikh13052001@gmail.com>
2024-05-31 12:20:08 +0530

    clear main

--- src/Hooks/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./useGetAll";
-export * from "./useCreateOrUpdate";
-export * from "./useDelete";
-export * from "./useDebounce";
-export * from "./useDownloadFile";
--- src/constants/button.js
+++
@@ -1,6 +0,0 @@
-import LoadMore from '../assets/LoadMore.png'
-import Button2 from '../assets/Button2.png'
-export default{
-    LoadMore,
-    Button2
-}
